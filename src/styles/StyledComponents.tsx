294cf294c3960e9fe55213fc43bfbf95d10e6968
Caio Mognatti <caiocsm97@gmail.com>
2023-06-27 15:29:35 -0300

    feat: adiciona responsividade aos componentes criados até o momento (Home page)

--- src/styles/StyledComponents.tsx
+++ src/styles/StyledComponents.tsx
@@ -59,7 +59,6 @@ export const Nav = styled.nav`
     padding: 16px 0;
   }
 `;
-
 export const LogoAndList = styled.div`
   display: flex;
   align-items: center;
@@ -121,7 +120,7 @@ export const SearchButton = styled.button`
   color: white;
   padding: 9px 12px;
   cursor: pointer;
-  @media screen {
+  @media (max-width: ${breakPoints.mobile}) {
     color: ${paleta.preto};
     border: 1px solid ${paleta.preto};
     padding: 9px 16.21px;
@@ -135,8 +134,14 @@ export const SearchInput = styled.input`
   }
 `;
 
-//Home
-export const HomeContainer = styled.section`
+//Pages - Home
+export const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+//BannerImages
+export const CarrosselContainer = styled.section`
   display: flex;
   flex-direction: row;
   margin-top: 40px;
@@ -186,4 +191,195 @@ export const RigthArrow = styled(TbPlayerPlayFilled)`
     transition: 350ms;
     transform: scale(1.2);
   }
+`;
+
+//Categories
+export const CategoriesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+export const CategoriesTitle = styled.h3`
+  font-weight: 500;
+  font-size: 32px;
+`;
+export const CategoriesList = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  gap: 30px;
+  @media (max-width: ${breakPoints.tablet}) {
+    flex-wrap: wrap;
+  }
+`;
+export const CategoriesItem = styled.li``;
+
+export const CategorisItemName = styled.p`
+  text-align: center;
+  color: ${paleta.branco};
+  background-color: ${paleta.preto};
+  padding: 8px;
+  margin: -8px 0 0 0;
+`;
+
+//Products
+export const ProductsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-top: 48px;
+`;
+export const ProductsTitle = styled.h3`
+  font-weight: 500;
+  font-size: 32px;
+  @media (max-width: ${breakPoints.mobile}) {
+    text-align: center;
+  }
+`;
+export const ProductsList = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 90%;
+  justify-content: center;
+  gap: 30px;
+  @media (max-width: ${breakPoints.tablet}) {
+    flex-wrap: wrap;
+    width: 100%;
+  }
+`;
+export const ProductsItem = styled.li`
+  border: 1px solid ${paleta.cinza};
+`;
+export const ProductsImage = styled.img`
+  width: 350px;
+  height: 422px;
+  @media (max-width: ${breakPoints.tablet}) {
+    width: 330px;
+  }
+`;
+export const ProductsItemInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 350px;
+  margin: 0;
+  text-align: justify;
+  @media (max-width: ${breakPoints.tablet}) {
+    width: 330px;
+  }
+`;
+export const ProductsItemInfoTitle = styled.p`
+  font-weight: bold;
+  padding: 6px 16px;
+  margin: 0;
+`;
+export const ProductsItemInfoDescriptionContainer = styled.div`
+  padding: 6px 16px;
+`;
+export const ProductsItemInfoDescription = styled.p`
+  font-size: 13px;
+  line-height: 20px;
+  margin: 0;
+`;
+export const ProductButtonContainer = styled.div`
+  padding: 0 0 16px 16px;
+`;
+export const ProductButton = styled.button`
+  background-color: ${paleta.roxo};
+  color: ${paleta.branco};
+  border: none;
+  padding: 9px 16px;
+`;
+
+//Facilidades
+export const FacilidadesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-top: 84px;
+  background-color: ${paleta.preto};
+  @media (max-width: ${breakPoints.mobile}) {
+    flex-wrap: wrap;
+    margin-top: 48px;
+  }
+`;
+export const FacilidadesTitle = styled.h3`
+  font-weight: 500;
+  font-size: 32px;
+  padding-bottom: 32px;
+
+  color: ${paleta.branco};
+  @media (max-width: ${breakPoints.mobile}) {
+    text-align: center;
+    font-size: 24px;
+    line-height: 29px;
+    margin-bottom: 0;
+  }
+`;
+export const FacilidadesList = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 80%;
+  justify-content: center;
+  gap: 30px;
+  @media (max-width: ${breakPoints.mobile}) {
+    flex-direction: column;
+    align-content: center;
+  }
+`;
+export const FacilidadesItem = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30%;
+  padding-bottom: 40px;
+  @media (max-width: ${breakPoints.mobile}) {
+    width: 100%;
+  }
+`;
+export const FacilidadesImage = styled.img`
+  width: 72px;
+  height: 72px;
+  @media (max-width: ${breakPoints.mobile}) {
+    width: 56px;
+    height: 56px;
+  }
+`;
+export const FacilidadesItemInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 350px;
+  margin: 0 24px;
+  text-align: justify;
+`;
+export const FacilidadesItemTitle = styled.h3`
+  color: ${paleta.amarelo};
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 23px;
+  margin: 0;
+`;
+export const FacilidadeItemDescription = styled.p`
+  color: ${paleta.branco};
+  margin: 0;
+  padding: 16px 0;
+  font-size: 13px;
+  font-weight: 400;
+  line-height: 20px;
 `;
