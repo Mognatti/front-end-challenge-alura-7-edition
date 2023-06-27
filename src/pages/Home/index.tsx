f10a743d56b89dee5f69e329626bb58d6bd181f3
Caio Mognatti <caiocsm97@gmail.com>
2023-06-27 15:26:01 -0300

    feat: Mudança na organização geral da página, para que cada componte tenha uma responsabilidade única.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,55 +1,19 @@
-import { useState } from "react";
-import {
-  BannerImg,
-  BannerLi,
-  CarrosselList,
-  HomeContainer,
-  ImageContainer,
-  LeftArrow,
-  RigthArrow,
-} from "../../styles/StyledComponents";
-import { banners } from "./BannerImages";
 import { useWindowSize } from "../../hooks/useWindowSize";
+import { HomeContainer } from "../../styles/StyledComponents";
+import CarrosselBanner from "./BannerImages";
+import Categories from "./Categoria";
+import { Facilidades } from "./Facilidades";
+import { Products } from "./Products";
 
 export default function Home() {
-  const [bannerAtual, setBannerAtual] = useState(0);
   const [{ windowWidth, tablet, mobile }] = useWindowSize();
-  const length = banners.length;
-
-  function nextBanner() {
-    setBannerAtual(bannerAtual === length - 1 ? 0 : bannerAtual + 1);
-  }
-
-  function prevBanner() {
-    setBannerAtual(bannerAtual === 0 ? length - 1 : bannerAtual - 1);
-  }
 
   return (
     <HomeContainer>
-      <ImageContainer>
-        <CarrosselList>
-          <LeftArrow onClick={() => prevBanner()} size="25" />
-          {banners.map((banner, index) => {
-            if (index === bannerAtual) {
-              return (
-                <BannerLi key={banner.nome}>
-                  <BannerImg
-                    src={
-                      windowWidth > tablet
-                        ? banner.srcDesktop
-                        : windowWidth <= tablet && windowWidth > mobile
-                        ? banner.srcTablet
-                        : banner.srcMobile
-                    }
-                    alt={banner.nome}
-                  />
-                </BannerLi>
-              );
-            }
-          })}
-          <RigthArrow onClick={() => nextBanner()} size="25" />
-        </CarrosselList>
-      </ImageContainer>
+      <CarrosselBanner {...{ windowWidth, tablet, mobile }} />
+      <Categories {...{ windowWidth, tablet, mobile }} />
+      <Products {...{ windowWidth, tablet, mobile }} />
+      <Facilidades {...{ windowWidth, tablet, mobile }} />
     </HomeContainer>
   );
 }
