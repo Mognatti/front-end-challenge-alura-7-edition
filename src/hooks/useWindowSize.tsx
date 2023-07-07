988c7c9a89f92b7dd55c04bf7d37c9d6d7d903aa
Caio Mognatti <caiocsm97@gmail.com>
2023-07-07 17:07:35 -0300

    fix: mudança no hook que percebe o tamano da janela - agora ustiliza outerWidth ao invés de innerWidth para prevenir alguns bugs

--- src/hooks/useWindowSize.tsx
+++ src/hooks/useWindowSize.tsx
@@ -1,14 +1,15 @@
 import { useState, useEffect } from "react";
 
 export function useWindowSize() {
-  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+  const [windowWidth, setWindowWidth] = useState<number>(window.outerWidth);
   const [showHamburguer, setShowHamburguer] = useState<boolean>();
   const tablet = 768;
   const mobile = 387;
 
   useEffect(() => {
-    setWindowWidth(window.innerWidth);
+    setWindowWidth(window.outerWidth);
     windowWidth <= mobile ? setShowHamburguer(true) : setShowHamburguer(false);
+
     window.addEventListener("resize", () => setWindowWidth(window.outerWidth));
   }, [windowWidth]);
 
